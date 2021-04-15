7390bc9950c662ade9fe917b64886be462b2e597
LivintLucian <livintlucian@gmail.com>
2021-04-15 13:18:15 +0300

    Mentor-profile-stylish

--- arThek/arThek- FrontEnd/arThek-app/src/app/app-routing.module.ts
+++ arThek/arThek- FrontEnd/arThek-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterJoinAsMentorComponent } from './register/register-join-as-mentor/register-join-as-mentor.component';
 import { RegisterJoinAsUserComponent } from './register/register-join-as-user/register-join-as-user.component';
 import { RegisterMentorAditionalDataComponent } from './register/register-mentor-aditional-data/register-mentor-aditional-data.component';
+import { RegisterMentorProfileComponent } from './register/register-mentor-profile/register-mentor-profile.component';
 import { RegisterMentorVolunteerTypeComponent } from './register/register-mentor-volunteer-type/register-mentor-volunteer-type.component';
 import { RegisterUserTypeComponent } from './register/register-user-type/register-user-type.component';
 
@@ -30,6 +31,10 @@ const routes: Routes = [
   {
     path: 'register/mentor/aditional-data',
     component: RegisterMentorAditionalDataComponent,
+  },
+  {
+    path: 'mentor/profile',
+    component: RegisterMentorProfileComponent,
   },
 ];
 
